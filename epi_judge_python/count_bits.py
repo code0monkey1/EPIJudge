bb0f79c3c737caf305dd8fa8ab3db2b1e0e409fe
Chiranjeev Thomas <chiranjeevwork@gmail.com>
2023-04-29 05:05:54 +0000

    29 Apr 2023, 10:35

--- epi_judge_python/count_bits.py
+++ epi_judge_python/count_bits.py
@@ -1,5 +1,7 @@
 from test_framework import generic_test
 
+# Do in O(n) , where n is the number of bits to  represent integer (Eg: 12 ( 1100 ) needs 4 
+# bits to represent integer , so here n = 4)
 
 def count_bits(x: int) -> int:
  pass
